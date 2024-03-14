67467df159d79df1e3820b60d44984357a602fae
Dwaraka3 <127289599+Dwaraka3@users.noreply.github.com>
2024-03-14 22:23:07 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+// const CORSPROXY = 'https://proxy.cors.sh/';
+
+export const LOCATION_API =
+  'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/misc/place-autocomplete?input=';
+
+export const ADDRESS_API =
+  'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/misc/address-recommend?place_id=';
+
+export const IMG_CAROUSEL =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/';
+
+export const LOGO_URL =
+  'https://img.freepik.com/free-vector/hand-drawn-cheetah-logo_23-2150571201.jpg?t=st=1710422802~exp=1710426402~hmac=8fc48093bc86bd4fde7176a18185842f34c017b7f019125bc5a03ef2171b4f2a&w=740';
+
+export const RES_IMG =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
+
+export const GRAY_RES_IMG =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/';
+
+export const MENU_API =
+  'https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.9747271&lng=88.43372099999999&restaurantId=';
+
+export const MENU_IMG =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/';
+
+export const CATEGORY_IMG =
+  'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/';
